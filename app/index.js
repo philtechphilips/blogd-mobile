e61b312602a69c4583ec4b3d11a5e495ecc52799
Pelumi Isola <C_isolapel@gcn.ab-inbev.com>
2024-06-11 23:58:30 +0100

    feat: app setup and home screen

---
+++ app/index.js
@@ -0,0 +1,116 @@
+import {
+  ImageBackground,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { hp, wp } from "../helpers/common";
+import { theme } from "../constants/theme";
+
+const Home = () => {
+  return (
+    <ImageBackground
+      source={require("../assets/images/side-view-radio-microphone-with-copy-space.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.overlay}>
+        <View style={styles.logo}>
+          <Text style={styles.logoText}>BLOGD</Text>
+        </View>
+
+        <View style={styles.content}>
+          <Text style={styles.headingText}>
+            Headlines and Tales at Your Fingertips
+          </Text>
+          <TouchableOpacity style={styles.login}>
+            <Text style={styles.logintext}>CONTINUE</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.register}>
+            <Text style={styles.registerText}>Create an account.</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <StatusBar style="auto" />
+    </ImageBackground>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  content: {
+    flex: 1,
+    paddingHorizontal: 25,
+    paddingVertical: 0,
+    width: wp(100),
+    borderTopEndRadius: 30,
+    borderTopStartRadius: 30,
+    gap: 32,
+    position: "absolute",
+    bottom: 50
+  },
+
+  logo: {
+    position: "absolute",
+    top: 60,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  logoText: {
+    color: theme.colors.white,
+    fontSize: hp(4),
+    fontWeight: theme.fontWeights.semibold,
+    textAlign: "center"
+  },
+
+  login: {
+    padding: 16,
+    alignItems: "center",
+    borderRadius: 8,
+    backgroundColor: "#FF6347",
+  },
+
+  register: {
+    backgroundColor: "transparent",
+    alignItems: "center",
+  },
+
+  logintext: {
+    color: theme.colors.white,
+    fontSize: 14,
+    fontWeight: "600",
+  },
+
+  registerText: {
+    color: theme.colors.white,
+    fontSize: 14,
+    fontWeight: "600",
+  },
+
+  headingText: {
+    color: theme.colors.white,
+    fontSize: 56,
+    fontWeight: "300",
+    textAlign: "left",
+    marginBottom: 60
+  },
+});
